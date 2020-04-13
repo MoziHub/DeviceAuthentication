b9c1c1e52902cac426f8e65939861efa2c5f58db
Zack Zhang <zack.zhang@salmet.de>
2020-04-13 15:23:36 +0800

    branch test

--- DeviceAuthAndUsage/DeviceAuthAndUsage/ViewController.swift
+++ DeviceAuthAndUsage/DeviceAuthAndUsage/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        print("TVOS")
         Camera().requestAuthentication(completion: { (result:Bool) in
 
         })
